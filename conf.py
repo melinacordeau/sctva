d0020aa3aecd6a77d268b509dd5f426e4572f6eb
Alex <alexandre.pron@gmail.com>
2020-04-18 13:27:17 +0200

    UPDATE : conf added usage

--- conf.py
+++ conf.py
@@ -2,4 +2,5 @@
 Configuration file used to store global variables like subjects name, database structure and locations
 Avoid to have multiple definitions into scripts files.
 Example : BANCO ='/hpc/banco'
+        from conf import BANCO (in scripts)
 """
