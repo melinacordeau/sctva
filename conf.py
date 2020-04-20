224ce2425d0019d10fd8e4f2c3bc5996dae96afb
Alex <alexandre.pron@gmail.com>
2020-04-20 16:56:22 +0200

    UPDATE : new variables in conf file and added script to test extraction of functionnal peaks

--- conf.py
+++ conf.py
@@ -11,12 +11,28 @@ N = 10
 SUBJECTS_PREFIX = 'sub-'
 SUBJECTS = (SUBJECTS_PREFIX + '0' + str(i) if i < 10 else SUBJECTS_PREFIX + str(i) for i in range(1, N, 1))
 
-BANCO = '/hpc/banco'
-BRAINVISA_DB = os.path.join(BANCO, 'tobecompleted')   # a completer
+
+
+BANCO_HPC = '/hpc/banco'   #frioul connection
+BANCO_LOCAL = r'L:\banco'  #local mount
+
+if os.path.exists(BANCO_HPC):
+    BANCO = BANCO_HPC
+else:
+    BANCO = BANCO_LOCAL
+
+PRIMAVOICE = os.path.join(BANCO, 'Primavoice_Data_and_Analysis')
+PRIM_DTI = os.path.join(PRIMAVOICE, 'DTI')
+CERIMED = os.path.join(PRIM_DTI, 'cerimed')
+SUBJS_DIR = {subject: os.path.join(CERIMED, subject) for subject in SUBJECTS}
+
+
+
+#BRAINVISA_DB = os.path.join(BANCO, 'tobecompleted')   # a completer
 CENTER = 'subjects'
 ACQUISITION = 'default_acquisition'
 CORRECTION = 'default_analysis'
 
 #Generate dictionnaries containing path of the files
-CORRECTED_DWI = {subject: os.path.join(BRAINVISA_DB, CENTER, subject, 'dmri', ACQUISITION, CORRECTION) for subject in SUBJECTS}
+#CORRECTED_DWI = {subject: os.path.join(BRAINVISA_DB, CENTER, subject, 'dmri', ACQUISITION, CORRECTION) for subject in SUBJECTS}
 
