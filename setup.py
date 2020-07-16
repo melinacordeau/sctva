008f69bc9f894382fba1e861507b08d49bbad4a0
Alex <alexandre.pron@gmail.com>
2020-07-16 11:07:03 +0200

    UPDATE: added setup.py file for package install

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+BASE_REQUIREMENTS = ["nipype", "nibabel","numpy", "scikit-image"]
+DEV_REQUIREMENTS = ["black", "flake8", "pytest", "pytest-cov", "codecov"]
+
+
+setup(
+    name="sctva",
+    version="0.0.0",
+    packages=find_packages(),
+    author="Alexandre Pron",
+    description="Structural Connectivity of Temporal Voice Areas",
+    url="https://github.com/alexpron/connectivity_tva",
+    license="MIT",
+    python_requires='>=3.6',  # enforce Python 3.6 as minimum
+    install_requires=BASE_REQUIREMENTS,
+    extras_require={"dev": DEV_REQUIREMENTS},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+    ],
+)
